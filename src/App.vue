83452cd52de178d256594d63fca66ac03d92171c
Mihai <haos-sah@mail.ru>
2022-06-28 20:13:08 +0300

    initialization

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationDrawer from "@/components/NavigationDrawer";
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    NavigationDrawer
+  },
+
+  data: () => ({
+
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <v-app>
+    <NavigationDrawer/>
+    <v-app-bar app/>
+    <v-main>
+<!--        <router-view></router-view>-->
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
